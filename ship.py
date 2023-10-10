1cc92b3c4266affff5f8c7188724ba8a1611ab48
kcao22 <kevincao222@gmail.com>
2023-10-09 19:12:39 -0700

    adjusted ship speed, added vertical and horizontal cont. movement functionality

--- ship.py
+++ ship.py
@@ -1,4 +1,5 @@
 import pygame
+from settings import Settings
 
 class Ship:
     """
@@ -8,43 +9,57 @@ class Ship:
         """
         Initializes ship and sets starting position.
         """
-        # All game elements in Pygame are rectangles.
-        # Access rectangles to place / position objects in game.
-        # Access game screen resources as defined in SpaceInvaders.
-        # Get screen's rectangle.
+        # All game elements in Pygame are rectangles
+        # Access rectangles to place / position objects in game
+        # Access game screen resources as defined in SpaceInvaders
+        # Get screen's rectangle
         self.screen = si_game.screen
         self.screen_rect = si_game.screen.get_rect()
         
-        # Image returns surface representing ship.
-        # Get ship's surface rectangle. 
-        # Set ship to middle bottom of game screen.
+        # Image returns surface representing ship
+        # Get ship's surface rectangle
+        # Set ship to middle bottom of game screen
         self.image = pygame.image.load("images/ship.bmp")
         self.rect = self.image.get_rect()
         self.rect.midbottom = self.screen_rect.midbottom
         
-        # Flag for continuous movements. If True, continuously += 1 pos.
+        # Set placeholder x and y positions as floats. Use to update rect
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
+        
+        # Flag for continuous movements. If True, continuously += 1 pos
         self.moving_right = False
         self.moving_left = False
         self.moving_up = False
         self.moving_down = False
         
+        # Access settings for ship speed
+        self.settings = Settings()
+        
+        
     def blitme(self):
         """
         Draws ship at current location.
         """
-        # Draws image to game screen at specified position by self.rect.
+        # Draws image to game screen at specified position by self.rect
         self.screen.blit(self.image, self.rect)
         
     def update(self):
         """
         Checks if continuous movement flag. Update position accordingly.
         """
-        # If moving_right, iterate ship rectangle position to right once.
+        # Iterate x, y based on flag
+        # If instead of elif to avoid single key registering only
+        # Ex: elif will prioritize right key if left and right are pushed
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.y += 1
+            self.x -= self.settings.ship_speed
         if self.moving_up:
-            self.rect.top += 1
+            self.y -= self.settings.ship_speed
         if self.moving_down:
-            self.rect.bottom += 1
+            self.y += self.settings.ship_speed
+        
+        # Update position of rectangle after iteration
+        self.rect.x = self.x
+        self.rect.y = self.y
--- space_invaders.py
+++ space_invaders.py
@@ -60,10 +60,21 @@ class SpaceInvaders:
                 # If right arrow key, shift ship rectangle 1 unit right
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+                elif event.key == pygame.K_UP:
+                    self.ship.moving_up = True
+                elif event.key == pygame.K_DOWN:
+                    self.ship.moving_down = True
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = False
-            
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+                elif event.key == pygame.K_UP:
+                    self.ship.moving_up = False
+                elif event.key == pygame.K_DOWN:
+                    self.ship.moving_down = False
                 
         
     def _update_screen(self):
